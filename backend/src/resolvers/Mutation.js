b9078bf11b2142fa02a5885a36ab2ebf2e110a58
Mitch Moore <mamoore9369@gmail.com>
2019-01-05 12:53:40 -0500

    Module 5-Video 24: Added backend functionality to sign up a new user;

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         //TODO: Check if they are logged in
@@ -36,6 +39,34 @@ const Mutations = {
 
         //3. delete it
         return ctx.db.mutation.deleteItem({ where }, info);
+    }, 
+    async signup(parent, args, ctx, info) {
+        //1. Lower case email to prevent issues later on
+        args.email = args.email.toLowerCase();
+
+        //2. Hash the password passed in
+        const password = await bcrypt.hash(args.password, 10); //10 is the length of the salt that bcrypt will be used
+    
+        //3. Create the user in the database
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args, //Spreads out args.name, args.email, and args.password
+                password: password, //overwrites password passed in from args
+                permissions: { set: ['USER'] },
+            }
+        }, info);
+
+        //4. Create JWT token for the new user since we already have the password
+        const token = jwt.sign({ userId: user.Id }, process.env.APP_SECRET);
+
+        //5. Set the JWT as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true, 
+            maxAge: 1000 * 60 * 60 * 24 * 365, //1 year
+        });
+
+        //6. Return the user to the browser
+        return user;
     }
 };
 
